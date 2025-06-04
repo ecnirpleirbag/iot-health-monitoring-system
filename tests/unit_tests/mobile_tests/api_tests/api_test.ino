800b9f623efc423767df6a84995677d7c5153804
Gabriel prince <77666971+ecnirpleirbag@users.noreply.github.com>
2025-06-04 16:42:17 +0530

    Update api_test.ino

--- tests/unit_tests/mobile_tests/api_tests/api_test.ino
+++ tests/unit_tests/mobile_tests/api_tests/api_test.ino
@@ -1,1 +1,9 @@
-// Code nneed to upload by Gabriel
+void setup() {
+  Serial.begin(115200);
+  Serial.println("API Test Mock: Sending dummy data...");
+}
+
+void loop() {
+  // Just a mock test file
+  delay(2000);
+}
