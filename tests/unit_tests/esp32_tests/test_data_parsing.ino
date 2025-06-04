362d950bb81f52542874fb527da01e0f45175895
Gabriel prince <77666971+ecnirpleirbag@users.noreply.github.com>
2025-06-04 16:41:41 +0530

    Update test_data_parsing.ino

--- tests/unit_tests/esp32_tests/test_data_parsing.ino
+++ tests/unit_tests/esp32_tests/test_data_parsing.ino
@@ -1,1 +1,9 @@
-// code needed to be uploaded by Gabriel
+void setup() {
+  Serial.begin(9600);
+  String data = "HR:78,TEMP:36.7";
+  int hrIndex = data.indexOf("HR:");
+  int tempIndex = data.indexOf(",TEMP:");
+  int hr = data.substring(hrIndex + 3, tempIndex).toInt();
+  float temp = data.substring(tempIndex + 6).toFloat();
+  Serial.printf("HR = %d, TEMP = %.1f\n", hr, temp);
+}
