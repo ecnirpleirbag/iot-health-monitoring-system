4cf5fb099b7d405c6dd1b1564facac4c3c7d18a9
Gabriel prince <77666971+ecnirpleirbag@users.noreply.github.com>
2025-06-04 16:40:48 +0530

    Update test_fall_detection.ino

--- tests/unit_tests/arduino_tests/test_fall_detection.ino
+++ tests/unit_tests/arduino_tests/test_fall_detection.ino
@@ -1,1 +1,11 @@
-// code need to be uploaded by Gabriel
+void setup() {
+  Serial.begin(9600);
+  pinMode(3, INPUT);
+}
+
+void loop() {
+  if (digitalRead(3) == LOW) {
+    Serial.println("Fall Detected");
+  }
+  delay(500);
+}
