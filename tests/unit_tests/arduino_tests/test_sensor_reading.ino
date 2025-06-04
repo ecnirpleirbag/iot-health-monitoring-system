4243c05906077f54ff979b17d1c9bda61c4b874a
Gabriel prince <77666971+ecnirpleirbag@users.noreply.github.com>
2025-06-04 16:41:02 +0530

    Update test_sensor_reading.ino

--- tests/unit_tests/arduino_tests/test_sensor_reading.ino
+++ tests/unit_tests/arduino_tests/test_sensor_reading.ino
@@ -1,1 +1,19 @@
-// code need to be uploaded by Gabriel
+#include <Wire.h>
+#include <Adafruit_MPU6050.h>
+
+Adafruit_MPU6050 mpu;
+
+void setup() {
+  Serial.begin(115200);
+  if (!mpu.begin()) {
+    Serial.println("MPU6050 init failed!");
+    while (1);
+  }
+}
+
+void loop() {
+  sensors_event_t a, g, temp;
+  mpu.getEvent(&a, &g, &temp);
+  Serial.printf("Accel: %.2f, %.2f, %.2f\n", a.acceleration.x, a.acceleration.y, a.acceleration.z);
+  delay(1000);
+}
