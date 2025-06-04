9da4bf81588ba268e0e9a1e86d4dba5109ab36b6
Gabriel prince <77666971+ecnirpleirbag@users.noreply.github.com>
2025-06-04 16:41:57 +0530

    Update test_wifi_connection.ino

--- tests/unit_tests/esp32_tests/test_wifi_connection.ino
+++ tests/unit_tests/esp32_tests/test_wifi_connection.ino
@@ -1,1 +1,11 @@
-// Code needed to be uploaded by Gabriel
+#include <WiFi.h>
+
+void setup() {
+  Serial.begin(115200);
+  WiFi.begin("SSID", "PASSWORD");
+  while (WiFi.status() != WL_CONNECTED) {
+    Serial.print(".");
+    delay(500);
+  }
+  Serial.println("\nConnected to WiFi");
+}
