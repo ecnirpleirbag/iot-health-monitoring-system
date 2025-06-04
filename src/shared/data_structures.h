c4986def849abce25fea5dacf4f15dddb44b1bb8
ecnirpleirbag <gabrielprince2k@gmail.com>
2025-06-04 12:32:50 +0530

    Add datastructures.h for shared data structure definitions

--- src/shared/data_structures.h
+++ src/shared/data_structures.h
@@ -1,1 +1,14 @@
 // code by Gabriel
+#ifndef DATA_STRUCTURES_H
+#define DATA_STRUCTURES_H
+
+struct SensorData {
+	int heartRateRaw;
+	float temperatureCelsius;
+	bool fallDetected;
+	float systolicBP;
+	float diastolicBP;
+};
+
+
+#endif
