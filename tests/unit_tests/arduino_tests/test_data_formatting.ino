58b4b732e9b97496b7ee2f0259dd3fb89ba55bd5
Gabriel prince <77666971+ecnirpleirbag@users.noreply.github.com>
2025-06-04 16:40:30 +0530

    Update test_data_formatting.ino

--- tests/unit_tests/arduino_tests/test_data_formatting.ino
+++ tests/unit_tests/arduino_tests/test_data_formatting.ino
@@ -1,1 +1,10 @@
-// Code need to be uploaded by Gabriel
+void setup() {
+  Serial.begin(9600);
+}
+
+void loop() {
+  float hr = 78.5;
+  float temp = 36.6;
+  Serial.printf("HR:%.1f,TEMP:%.1f\n", hr, temp);
+  delay(1000);
+}
