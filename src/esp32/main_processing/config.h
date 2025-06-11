e78dc5bbc11cecacda2e7a7a09a7c5c26c7c1de2
Gabriel prince <77666971+ecnirpleirbag@users.noreply.github.com>
2025-06-11 11:57:13 +0530

    Update config.h

--- src/esp32/main_processing/config.h
+++ src/esp32/main_processing/config.h
@@ -1,1 +1,10 @@
-// code need to be uploaded by Khariharan
+#ifndef CONFIG_H
+#define CONFIG_H
+
+#define WIFI_SSID "YourSSID"
+#define WIFI_PASSWORD "YourPassword"
+
+#define CLOUD_ENDPOINT "http://your-server.com/api/upload"
+#define DEVICE_ID "ESP32_HEALTH_MONITOR"
+
+#endif
