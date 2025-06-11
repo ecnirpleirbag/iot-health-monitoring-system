ea54e292929c39e4520032dc993df7a1999e6186
Gabriel prince <77666971+ecnirpleirbag@users.noreply.github.com>
2025-06-11 11:57:27 +0530

    Update cloud_communication.h

--- src/esp32/main_processing/cloud_communication.h
+++ src/esp32/main_processing/cloud_communication.h
@@ -1,1 +1,10 @@
-// Khariharan
+#ifndef CLOUD_COMMUNICATION_H
+#define CLOUD_COMMUNICATION_H
+
+#include <WiFi.h>
+#include <HTTPClient.h>
+
+void connectToWiFi();
+void uploadData(float temperature, int heartRate, float systolic, float diastolic);
+
+#endif
