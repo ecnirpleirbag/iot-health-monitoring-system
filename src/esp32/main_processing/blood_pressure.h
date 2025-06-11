e92096a7cd84fdc9c3c765b37e4f37597359cc79
Gabriel prince <77666971+ecnirpleirbag@users.noreply.github.com>
2025-06-11 11:57:44 +0530

    Update blood_pressure.h

--- src/esp32/main_processing/blood_pressure.h
+++ src/esp32/main_processing/blood_pressure.h
@@ -1,1 +1,7 @@
-// Khariharan
+#ifndef BLOOD_PRESSURE_H
+#define BLOOD_PRESSURE_H
+
+float calculatePTT(unsigned long ecgTime, unsigned long ppgTime);
+void estimateBloodPressure(float ptt, float &systolic, float &diastolic);
+
+#endif
