014733a19d41446387e72d86b7515591b5f9eb43
Gabriel prince <77666971+ecnirpleirbag@users.noreply.github.com>
2025-06-11 11:56:50 +0530

    Update sensor_acquisition.ino

--- src/arduino/sensor_acquisition/sensor_acquisition.ino
+++ src/arduino/sensor_acquisition/sensor_acquisition.ino
@@ -1,1 +1,21 @@
-// code to be uploaded by Arjun
+#include "config.h"
+#include "sensor_definitions.h"
+
+DHT dht(DHT_PIN, DHTTYPE);
+
+void setup() {
+  Serial.begin(115200);
+  setupSensors();
+}
+
+void loop() {
+  float temp = readTemperature();
+  int hr = readHeartRate();
+  float ecg = readECGSignal();
+
+  Serial.print("Temp: "); Serial.print(temp); Serial.print(" *C, ");
+  Serial.print("Heart Rate: "); Serial.print(hr); Serial.print(" bpm, ");
+  Serial.print("ECG: "); Serial.println(ecg);
+
+  delay(SENSOR_UPDATE_INTERVAL);
+}
