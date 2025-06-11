92fd18ed8ddd851adf75f54c7d67ff0a0e756765
Gabriel prince <77666971+ecnirpleirbag@users.noreply.github.com>
2025-06-11 11:56:30 +0530

    Update sensor_definitions.h

--- src/arduino/sensor_acquisition/sensor_definitions.h
+++ src/arduino/sensor_acquisition/sensor_definitions.h
@@ -1,1 +1,15 @@
-// Code need to uploaded by Arjun
+#ifndef SENSOR_DEFINITIONS_H
+#define SENSOR_DEFINITIONS_H
+
+#include <DHT.h>
+
+#define DHTTYPE DHT11
+
+extern DHT dht;
+
+void setupSensors();
+float readTemperature();
+int readHeartRate();
+float readECGSignal();
+
+#endif
